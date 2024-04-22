3e9fbd12648b8e05cf72fab72751754155a77668
AirStair <AirStair@users.noreply.github.com>
2024-04-22 11:56:14 +0300

    Update proxy-lib.mjs

--- proxy-lib.mjs
+++ proxy-lib.mjs
@@ -13,6 +13,9 @@ const getBody = incomingMessage => {
 };
 
 export const createProxy = options => proxyOptions => {
+    const protocol = 'http:';
+    const port = options?.port || 3030;
+    const host = options?.host || '127.0.0.1';
     const server = createServer(async (incomingMessage, serverResponse) => {
         const path = incomingMessage.url;
         const option = proxyOptions.find(option => {
@@ -20,34 +23,91 @@ export const createProxy = options => proxyOptions => {
         });
         if (!option) {
             serverResponse.statusCode = 404;
-            serverResponse.end('no route');
+            serverResponse.end('NO ROUTE');
             return;
         }
-        const currentRequest = request({
+        console.table('__INCOMING_MESSAGE__');
+        console.table([
+            {
+                protocol,
+                host,
+                port,
+                path,
+                method: incomingMessage.method,
+                pattern: option.pattern
+            }
+        ]);
+        console.group();
+        const body = await getBody(incomingMessage);
+        const content = JSON.parse(body || '{}');
+        console.log('__INCOMING_MESSAGE_BODY__');
+        console.table([
+            content
+        ]);
+
+        const clientRequest = request({
             host: option.host,
             port: option.port,
-            protocol: 'http:',
+            protocol,
             path,
             method: incomingMessage.method,
             headers: incomingMessage.headers
         }, async response => {
+            console.group();
             const body = await getBody(response);
+            const content = JSON.parse(body || '{}');
+            console.log('__SERVER_RESPONSE__');
+            console.table([
+                {
+                    protocol,
+                    host: option.host,
+                    port: option.port,
+                    path,
+                    method: incomingMessage.method,
+                    pattern: option.pattern
+                }
+            ]);
+            console.log('__SERVER_RESPONSE_BODY__');
+            console.table([
+                content
+            ]);
             serverResponse.end(body);
+            console.groupEnd();
         });
-        const body = await getBody(incomingMessage);
-        currentRequest.end(body);
+        clientRequest.end(body);
+        console.groupEnd();
     });
-
-    const port = options?.port || 3030;
-    const host = options?.host || '127.0.0.1';
 
     server.listen(port, host, () => {
         console.log(`proxy listen http://${host}:${port}`);
+        console.table([
+            {
+                protocol,
+                host,
+                port
+            }
+        ]);
     });
 
-    process.on('exit', () => {
+    const close = () => {
         server.close(() => {
             console.log(`proxy closed: http://${host}:${port}`);
+            console.table([
+                {
+                    protocol,
+                    host,
+                    port
+                }
+            ]);
         });
+    };
+
+    process.on('exit', () => {
+        close();
+    });
+
+    server.on('clientError', error => {
+        console.log(error);
+        close();
     });
 };
